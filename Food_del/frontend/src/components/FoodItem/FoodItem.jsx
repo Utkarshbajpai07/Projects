b80307250cb26bfc453315fa7ca27d8347565cec
Utkarshbajpai07 <utkarshb928@gmail.com>
2024-10-21 00:48:39 +0530

    Food List

--- Food_del/frontend/src/components/FoodItem/FoodItem.jsx
+++ Food_del/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,23 +1,27 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import './FoodItem.css'
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 // eslint-disable-next-line react/prop-types
-const FoodItem = ({name,price,description,image}) => {
-  const[itemCount,setItemCount]=useState(0)
+const FoodItem = ({id,name,price,description,image}) => {
+  
+  // eslint-disable-next-line no-unused-vars
+  const{cartItems,addToCart,removeFromCart}=useContext(StoreContext);
   return (
     <div className='food-item'>
         <div className="food-itme-img-container">
             <img src={image} alt="" className="food-item-image" />
             {
-              !itemCount
-                ?<img className='add' onClick = {()=>setItemCount(prev=>prev+1)}
-                src={assets.add_icon_white}/>
-                :<div className='food-item-counter'>
-                  <img onClick={()=>setItemCount(prev=>prev-1)} src={assets.remove_icon_red} alt="" />
-                  <p>{itemCount}</p>
-                  <img onClick={()=>setItemCount(prev=>prev+1)} src={assets.add_icon_green} alt="" />
+              !cartItems[id]
+                ?(<img className='add' onClick = {()=>addToCart(id)}
+                src={assets.add_icon_white}/>)
+                :(<div className='food-item-counter'>
+                  <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                  <p>{cartItems[id]}</p>
+                  <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt="" />
                   </div>
+                )
 
             }
         </div>
