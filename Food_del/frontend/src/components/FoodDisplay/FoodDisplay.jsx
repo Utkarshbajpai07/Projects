cce6b5eb3ab81dde50622b919bdcd0c61353906a
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-02-05 12:01:56 +0530

    Your commit message

--- Food_del/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food_del/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useContext } from 'react';
 import './FoodDisplay.css'
 import {StoreContext} from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem';
+// eslint-disable-next-line react/prop-types
 const FoodDisplay = ({category}) => {
 
     const {food_list}=useContext(StoreContext)
@@ -13,7 +15,10 @@ const FoodDisplay = ({category}) => {
       </h2>
       <div className="food-display-list"> 
         {food_list.map((item,index)=>{
+          // eslint-disable-next-line no-cond-assign, no-constant-condition, no-constant-binary-expression
+          if (category==="All" || category===item.category ){ 
             return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+           }
         })}
       </div>
       
