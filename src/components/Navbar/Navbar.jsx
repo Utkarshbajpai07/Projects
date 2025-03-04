e697e63f5d96d204131362f5a63c08c8ee6e9b0d
Utkarshbajpai07 <utkarshb928@gmail.com>
2025-03-04 22:34:31 +0530

    Cart Page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import React,{useState} from 'react';
+import React,{useContext, useState} from 'react';
 import './Navbar.css'
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
+
 const Navbar = ({setShowLogin}) => {
 
-  const [Menu,setMenu]=useState("Home")
-
+  const [Menu,setMenu]=useState("menu")
+  const {getTotalCartAmount} =useContext(StoreContext);
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className="logo" />
+      <Link to ='/' ><img src={assets.logo} alt="" className="logo" /></Link>
       <ul className="navbar_menu">
         <Link to='/' onClick={()=>setMenu("Home")} className={Menu==="Home"?"active":""}>Home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={Menu==="Menu"?"active":""}>Menu</a>
@@ -19,8 +21,8 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src= {assets.search_icon}alt="" />
         <div className="navbar-search_icon">
-            <img src={assets.basket_icon} alt="" />
-            <div className="dot"></div>
+            <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+            <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sing In</button>
       </div>
